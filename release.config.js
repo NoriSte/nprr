3980edc4ef0125a6b4159e74da3807060d46bb21
NoriSte <nori.ste.magni@gmail.com>
2019-10-14 17:05:45 +0200

    ci: add some semantic-release plugins

--- release.config.js
+++ release.config.js
@@ -1,7 +1,28 @@
 module.exports = {
   plugins: [
-    "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/npm"
+    "@semantic-release/github",
+    "@semantic-release/npm",
+    [
+      "semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+        }
+      }
+    ],
+    [
+      "semantic-release/release-notes-generator",
+      {
+        preset: "angular",
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+        },
+        writerOpts: {
+          commitsSort: ["subject", "scope"]
+        }
+      }
+    ]
   ]
 };
