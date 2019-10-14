296a034b5df2ac22855c64b878fbcba5b3509b22
NoriSte <nori.ste.magni@gmail.com>
2019-10-14 11:09:27 +0200

    feat: log the NPM project

--- src/read-npm-scripts.js
+++ src/read-npm-scripts.js
@@ -5,7 +5,11 @@ const npm = require("npm");
 const readNpmScripts = () =>
   new Promise(resolve => {
     npm.load(() => {
+      if (!npm.config.sources.project.path) {
+        throw new Error("No NPM project found");
+      }
       const packageJsonPath = npm.config.sources.project.path.replace(".npmrc", "package.json");
+      console.log(`NPM project: ${packageJsonPath}`);
       const packageJson = require(packageJsonPath);
       resolve(packageJson.scripts);
     });
