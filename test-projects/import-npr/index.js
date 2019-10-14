0a3f93f6a981c73c498b8923f487936923c7f52b
NoriSte <nori.ste.magni@gmail.com>
2019-10-14 10:17:20 +0200

    test: test the local installation too

---
+++ test-projects/import-npr/index.js
@@ -0,0 +1,6 @@
+require("npr")("test");
+require("npr")("npr test");
+require("npr")("npm run test");
+require("npr")(["test"]);
+require("npr")(["npr", "test"]);
+require("npr")(["npm", "run", "test"]);
